95ca186770eb35287b39b0dc26902104ca395728
darkphoenix <26391122+christytreviranus@users.noreply.github.com>
2019-08-27 18:08:21 -0700

    Fix heroku error with connection to db

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,15 @@
 //MySQL
 const mysql = require('mysql');
+const connection;
 
 //Use Dotenv package
 require("dotenv").config();
 
 if (process.env.JAWSDB_URL){
-    connection.mysql.createConnection(process.env.JAWSDB_URL);
+   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 //Create Connection to database
-const connection = mysql.createConnection({
+connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: process.env.MYSQL_UN,
